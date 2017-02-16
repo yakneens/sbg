ff7ee839000a27cdf03dd466bbd32f54b7f6d5e7
Sergei Iakhnin <llevar@gmail.com>
2017-02-16 18:02:15 +0100

    updates

--- gckr_genotype.py
+++ gckr_genotype.py
@@ -4,6 +4,7 @@ from sevenbridges.http.error_handlers import *
 import datetime
 import binpacking
 import re
+import os
 
 print("SBG library imported.")
 prof = 'cgc'
@@ -19,32 +20,25 @@ input_port_reference = "reference"
 input_port_bam_file = "BAMfile"
 input_port_bai_file = "BAIfile"
 
-tasks = [t for t in api.tasks.query(project=my_project, limit=100).all() if (t.status == "COMPLETED")]
-
-# input_port = "SPECIFY_INPUT_PORT_HERE"
-# for t in tasks:
-#     files_processed = t.inputs[input_port]
-#     for f in files_processed:
-#         if 'ProcessedFiles' not in f.tags: 
-#             f.tags = ['ProcessedFiles']
-#             f.save()
-#     print("Tagged %d files for task %s" %(len(files_processed),t.name))
-
-file_size_limit = 6 *1024 * 1024 * 1024
-
 reference_list = ['GRCh37-lite', 'GRCh37-lite-+-HPV_Redux-build', 'GRCh37-lite_WUGSC_variant_1', 'GRCh37-lite_WUGSC_variant_2', 'HG19_Broad_variant']
 bam = "BAM"
 bam_index = "BAI"
 
 bams = [f for f in api.files.query(project=my_project.id,limit=100, metadata={'reference_genome':reference_list, 'data_format': bam }).all()]
-bam_indices = [f for f in api.files.query(project=my_project.id,limit=100, metadata={'reference_genome':reference_list, 'data_format': bam_index = "BAI"
- }).all()]
+bams_by_sample = {bam_file.metadata['sample_uuid']: bam_file for bam_file in bams}
+bams_by_filename = {os.path.splitext(bam_file.name)[0]: bam_file for bam_file in bams_by_sample.values()}
+
+bam_indices = [f for f in api.files.query(project=my_project.id,limit=100, metadata={'reference_genome':reference_list, 'data_format': bam_index}).all()]
+bam_indices_by_filename = {os.path.splitext(bam_index.name)[0]: bam_index for bam_index in bam_indices}
+dupe_keys = set(bam_indices_by_filename.keys()) - set(bams_by_filename.keys())
+
+
 reference_file = api.files.get(id='5890a37ee4b03eab16190778')
 
 
 # get the ID of the app. The ID of the APP is your username/projectname/appID
 # You can find that from the PAGE url for your app/workflow in question. 
-app = api.apps.get(id="s_iakhnin/gckr_lof/scatter_freebayes")
+app = api.apps.get(id="s_iakhnin/gckr-lof/scatter-freebayes/10")
 
 # Inputs need to be set to inputs for the App you are running. 
 # If there are certain fixed inputs, you can set them outside the for loop
@@ -53,11 +47,11 @@ inputs = {}
 input_files = bams[0:6]
 input_indices = bam_indices[0:6]
  
-task_name = 'API task test')
+task_name = 'API task test'
 
 inputs[input_port_bam_file] = input_files
 inputs[input_port_bai_file] = input_indices
-
+inputs[input_port_reference] = reference_file
 my_task = api.tasks.create(name=task_name, project=my_project, 
                           app=app, inputs=inputs, run=False)
 if my_task.errors:
